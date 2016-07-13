55d3734a4ecab3775c34059e854aa4e6fae02976
Kirk Chen <rwk0119@yahoo.com.tw>
2016-07-13 09:15:57 +0800

    Implement second scenario

--- src/services/potterShoppingCart.ts
+++ src/services/potterShoppingCart.ts
@@ -1,11 +1,14 @@
 import { Book } from '../models/book';
+import * as _ from 'lodash';
 
 export default class PotterShoppingCart {
     Calculate(books: Book[]): number {
-        var totalPrice = 0;
-       for (var book of books) {
-           totalPrice += book.Count * 100;
-       }
+        var totalPrice = _.sumBy(books, (book) => book.Count * 100);
+
+        var bookCount = _.sumBy(books, 'Count');
+        if (bookCount >= 2) {
+            totalPrice = totalPrice * 0.95;
+        }
 
         return totalPrice;
     }
