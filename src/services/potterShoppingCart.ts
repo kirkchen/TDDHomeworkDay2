313360dc92ef97d38416a3b42107de5aec172e10
Kirk Chen <rwk0119@yahoo.com.tw>
2016-07-13 09:53:18 +0800

    Implement 6th scenario

--- src/services/potterShoppingCart.ts
+++ src/services/potterShoppingCart.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../typings/modules/lodash/index.d.ts" />
+
 import { Book } from '../models/book';
 import { DiscountRule } from '../models/discountRule';
 import * as _ from 'lodash';
@@ -8,17 +10,36 @@ export default class PotterShoppingCart {
         { Count: 2, DiscountRate: 0.95 },
         { Count: 3, DiscountRate: 0.9 },
         { Count: 4, DiscountRate: 0.8 },
-        { Count: 5, DiscountRate: 0.75 },               
+        { Count: 5, DiscountRate: 0.75 },
     ]
 
-    Calculate(books: Book[]): number {
-        var totalPrice = _.sumBy(books, (book) => book.Count * 100);
+    Calculate(books: Book[]): number {        
+        var bookGroupCounts: number[] = [];
 
-        var bookCount = _.sumBy(books, 'Count');
-        var discountRule = _.find(this.DiscountRule, ['Count', bookCount]);
-        if(discountRule){
-            totalPrice *= discountRule.DiscountRate;
-        }
+        //// Group books count by episode series
+        while (_.sumBy(books, 'Count') > 0) {
+            let bookGroupCount = 0;
+
+            for (var book of books) {
+                if (book.Count > 0) {
+                    book.Count--;
+                    bookGroupCount++;
+                }
+            }
+
+            bookGroupCounts.push(bookGroupCount);
+        }        
+
+        //// Calculate each series' price
+        var prices = bookGroupCounts.map((count) => {            
+            let discountRate = _.find(this.DiscountRule, ['Count', count]);
+            let price = count * 100 * discountRate.DiscountRate;
+
+            return price;
+        });
+
+        //// Calculate Total Price
+        var totalPrice = _.sum(prices);
 
         return totalPrice;
     }
